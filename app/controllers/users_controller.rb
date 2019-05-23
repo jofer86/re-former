7e69f354c057b1acd13e238de5a3769f2dc424d4
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-05-23 21:33:30 +0100

    add code for create method and verfy if the insertion works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    
+    @user = User.new(username: params[:user][:username],email: params[:user][:email],password: params[:user][:password])
+    if @user.save
+      redirect_to new_user_path
+    else 
+      render 'new'
+    end
   end
 end
